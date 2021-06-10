1b23611cb9ccb008acd81b4cc185106e55affb0d
DJ Taylor <dtaylor88@gmail.com>
2021-06-10 04:25:37 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,2 +1,20 @@
 class Doctor
+  @@all = []
+  attr_accessor :name, :patient 
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end
+  
+  def self.all 
+    @@all
+  end 
+  
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end 
+  
+  
+  
 end 
